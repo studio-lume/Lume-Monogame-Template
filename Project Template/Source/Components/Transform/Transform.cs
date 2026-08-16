e6bb6820b743e4207c66e0dee3ee3306b06efa73
Hugo Deserno <desernohugo@gmail.com>
2026-08-16 12:22:28 +0200

    feat: localPosition

--- Project Template/Source/Components/Transform/Transform.cs
+++ Project Template/Source/Components/Transform/Transform.cs
@@ -1,17 +1,34 @@
+using System;
 using Microsoft.Xna.Framework;
 using Project_Template.Source.Core;
 
 namespace Project_Template.Source.Components.Transform {
     public class Transform
         : ComponentBase {
-        public Vector2I Position { get; set; } = Vector2I.Zero;
+        public Vector2I WorldPosition { get; set; } = Vector2I.Zero;
+
+        public Vector2I LocalPosition {
+            get => WorldPosition;
+            set {
+                var radians = MathHelper.ToRadians(Rotation);
+
+                var cos = MathF.Cos(radians);
+                var sin = MathF.Sin(radians);
+
+                var x = (int)(value.X * cos - value.Y * sin);
+                var y = (int)(value.X * sin + value.Y * cos);
+
+                WorldPosition += new Vector2I(x, y);
+            }
+        }
+
         public Vector2I Size { get; set; } = new(100, 100);
         public float Rotation { get; set; }
 
         public Rectangle Bounds =>
             new(
-                Position.X,
-                Position.Y,
+                WorldPosition.X,
+                WorldPosition.Y,
                 Size.X,
                 Size.Y
             );
